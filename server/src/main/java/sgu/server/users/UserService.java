341298a98a2d4680f5c2343cf4eead34dae3341f
Leyla-Villanueva <leylavillanueva0205@gmail.com>
2025-10-25 21:03:06 -0600

    [ADD]CRUD users

---
+++ server/src/main/java/sgu/server/users/UserService.java
@@ -0,0 +1,43 @@
+package sgu.server.users;
+
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User userData) {
+        return userRepository.findById(id).map(user -> {
+            user.setName(userData.getName());
+            user.setEmail(userData.getEmail());
+            user.setPhone(userData.getPhone());
+            return userRepository.save(user);
+        }).orElse(null);
+    }
+
+    public boolean deleteUser(Long id) {
+        if (!userRepository.existsById(id)) return false;
+        userRepository.deleteById(id);
+        return true;
+    }
+}
